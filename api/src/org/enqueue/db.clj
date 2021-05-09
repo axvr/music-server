3b15a9e351db85ed23134df35859a9ddf6c6d986
Alex Vear <alex@vear.uk>
2021-05-09 19:12:08 +0100

    Switch to PostgreSQL and HoneySQL 2

--- api/src/org/enqueue/db.clj
+++ api/src/org/enqueue/db.clj
@@ -1,15 +1,18 @@
 (ns org.enqueue.db
   (:require [next.jdbc :as jdbc]
-            [next.jdbc.sql :as sql]
-            [honeysql.core :as honey]
+            next.jdbc.date-time
+            [honey.sql :as sql]
             ragtime.jdbc
             ragtime.repl))
 
 
-;; TODO: DB credentials.
+;; TODO: Get per-environment settings from EDN.
 (def db-spec
-  {:dbtype "sqlite"
-   :dbname "resources/example.db"})
+  {:dbtype "postgresql"
+   :dbname "enqueue"
+   :host "localhost"
+   :user "postgres"
+   :password "postgres"})
 
 
 (def ds (jdbc/get-datasource db-spec))
@@ -17,30 +20,6 @@
 
 ;;; -----------------------------------------------------------
 ;;; DB interaction functions
-
-
-;; TODO: doc strings.
-(def insert! (partial sql/insert! ds))
-(def insert-multi! (partial sql/insert-multi! ds))
-(def delete! (partial sql/delete! ds))
-(def update! (partial sql/update! ds))
-(def get-by-id (partial sql/get-by-id ds))
-(def find-by-keys (partial sql/find-by-keys ds))
-(def query (partial sql/query ds))
-
-
-(defn execute!
-  ([sql]
-   (jdbc/execute! ds (honey/format sql)))
-  ([sql opts]
-   (jdbc/execute! ds (honey/format sql) opts)))
-
-
-(defn execute-one!
-  ([sql]
-   (jdbc/execute-one! ds (honey/format sql)))
-  ([sql opts]
-   (jdbc/execute-one! ds (honey/format sql) opts)))
 
 
 (comment
@@ -60,8 +39,9 @@
 
 
 (def ragtime-config
-  {:datastore (ragtime.jdbc/sql-database db-spec)
+  {:datastore  (ragtime.jdbc/sql-database db-spec)
    :migrations (ragtime.jdbc/load-resources "migrations")})
+;; NOTE: default migration history table is "ragtime_migrations"
 
 
 (defn migrate []
