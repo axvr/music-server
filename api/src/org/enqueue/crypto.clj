3a31f45675c34ac9f1d016074f8c8047abf150b5
Alex Vear <alex@vear.uk>
2021-05-16 20:02:24 +0100

    Add authentication token crypto functions

--- api/src/org/enqueue/crypto.clj
+++ api/src/org/enqueue/crypto.clj
@@ -1,5 +1,8 @@
 (ns org.enqueue.crypto
-  (:require [caesium.crypto.pwhash :as pwhash]))
+  "Core cryptographic functions for Enqueue."
+  (:require [caesium.crypto.pwhash :as pwhash]
+            [caesium.crypto.auth :as auth]
+            [caesium.randombytes :as rb]))
 
 
 (defn base64-encode
@@ -32,10 +35,56 @@
     "US-ASCII"))
 
 
-(defn verify-password
+(defn valid-password?
   "Checks if a given password matches a Base64 encoded password hash.  Used for
   user authentication."
   [password-hash password]
   (= 0 (pwhash/pwhash-str-verify
          (base64-decode password-hash "US-ASCII")
          password)))
+
+
+(defn- bytebuffer->string
+  "Convert the bytebuffers returned by caesium into Java strings."
+  ([bb]
+   (bytebuffer->string bb "ISO-8859-1"))
+  ([bb charset]
+   (String. bb charset)))
+
+
+(defn- string->bytebuffer
+  "Convert a Java string into a bytebuffer suitable for use by caesium."
+  ([s]
+   (string->bytebuffer s "ISO-8859-1"))
+  ([s charset]
+   (.getBytes s charset)))
+
+
+(defn gen-signing-key
+  "Generate a random Base64 encoded signing key for the HMAC-SHA512-256
+  algorithm.  Used by the sign-message and valid-signature? functions."
+  []
+  (-> auth/hmacsha512256-keybytes
+      rb/randombytes
+      bytebuffer->string
+      (base64-encode "ISO-8859-1")))
+
+
+(defn sign-message
+  "Generate a HMAC-SHA512-256 signature for a message using the given key."
+  [key msg]
+  (let [k (string->bytebuffer (base64-decode key "ISO-8859-1"))]
+    (base64-encode
+      (bytebuffer->string
+        (auth/hmacsha512256 msg k))
+      "ISO-8859-1")))
+
+
+(defn valid-signature?
+  "Verify that a HMAC-SHA512-256 generated signature (sig) is valid for the
+  given message (msg)."
+  [key msg sig]
+  (try
+    (= sig (sign-message key msg))
+    (catch Throwable _
+      false)))
--- api/test/org/enqueue/crypto_test.clj
+++ api/test/org/enqueue/crypto_test.clj
@@ -37,11 +37,63 @@
 
 (deftest hash-password--hashed+can-verify
   (testing "password hashing works and is verifiable"
-    (dotimes [i 10]
+    (dotimes [_ 10]
       (let [password (rand-unicode-string)
             hashed-password (crypto/hash-password password)]
+        ;; Base64 encoded password-hash does not end with ASCII NULL characters.
         (is (not (str/ends-with? hashed-password "\u0000")))
+        ;; Password hashing didn't return input password.
         (is (not (= password hashed-password)))
-        (is (= 172 (count hashed-password)))
         (is (not (= (crypto/base64-encode password) hashed-password)))
-        (is (crypto/verify-password hashed-password password))))))
+        ;; Base64 encoded password is 172 characters long.
+        (is (= 172 (count hashed-password)))
+        ;; Hashed password is 128 characters long.
+        (is (= 128 (count (crypto/base64-decode hashed-password "US-ASCII"))))
+        ;; Password verification works on correct password.
+        (is (crypto/valid-password? hashed-password password))
+        ;; Password verification fails on invalid password.
+        (is (not (crypto/valid-password? hashed-password
+                                         (rand-unicode-string))))))))
+
+
+(deftest generate-valid-signing-key
+  (testing "generates valid signing key"
+    (dotimes [_ 10]
+      (let [key (crypto/gen-signing-key)]
+        ;; Base 64 encoded key is 44 chars long.
+        (is (= 44 (count key)))
+        ;; Actual key is 32 bytes long.
+        (is (= 32 (count (crypto/base64-decode key "ISO-8859-1"))))
+        ;; Base64 decodes key to same result every time.
+        (is (= (crypto/base64-decode key "ISO-8859-1")
+               (crypto/base64-decode key "ISO-8859-1")))
+        ;; Will not generate same on next run.
+        (is (not (= key (crypto/gen-signing-key))))))))
+
+
+(deftest generates-verifible-signature
+  (testing "generates verifyable signature"
+    (dotimes [_ 10]
+      (let [key (crypto/gen-signing-key)
+            msg (rand-unicode-string)
+            sig (crypto/sign-message key msg)]
+        ;; Base 64 encoded signature is 44 chars long.
+        (is (= 44 (count sig)))
+        ;; Actual signature is 32 bytes long.
+        (is (= 32 (count (crypto/base64-decode sig "ISO-8859-1"))))
+        ;; Signature is not the key.
+        (is (not (= sig key)))
+        ;; Signature is not the message.
+        (is (not (= sig msg)))
+        ;; Signature verification succeeds.
+        (is (crypto/valid-signature? key msg sig))
+        ;; Signature verification fails on modified message.
+        (is (not (crypto/valid-signature? key (rand-unicode-string) sig)))
+        ;; Signature verification fails on different key.
+        (is (not (crypto/valid-signature? (crypto/gen-signing-key) msg sig)))
+        ;; Signature verification fails on invalid key.
+        (is (not (crypto/valid-signature? nil msg sig)))
+        ;; Signature verification fails on invalid signature.
+        (is (not (crypto/valid-signature? key msg nil)))
+        ;; Signature verification fails on modified signature.
+        (is (not (crypto/valid-signature? key msg (crypto/sign-message key "Foo"))))))))
