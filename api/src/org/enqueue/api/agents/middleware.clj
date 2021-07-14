0ea1a68c68bccfa3d77648bd5c3ac85ed16635f2
Alex Vear <alex@vear.uk>
2021-07-14 19:02:24 +0100

    Fix wrap-auth middleware missing signing key

--- api/src/org/enqueue/api/agents/middleware.clj
+++ api/src/org/enqueue/api/agents/middleware.clj
@@ -1,17 +1,18 @@
 (ns org.enqueue.api.agents.middleware
-  (:require [org.enqueue.api.agents.eat :as eat]))
+  (:require [org.enqueue.api.agents.eat :as eat]
+            [org.enqueue.api.settings :as settings]))
 
 
 (defn wrap-auth
   "Wrap handler with token extraction.  If token is invalid or missing,
   responds with HTTP status 401."
-  [handler key]
+  [handler]
   (fn
     ([request]
-     (if-let [token (eat/extract-token request key)]
+     (if-let [token (eat/extract-token request settings/signing-key)]
        (handler (assoc request :token token))
        {:status 401}))
     ([request respond raise]
-     (if-let [token (eat/extract-token request key)]
+     (if-let [token (eat/extract-token request settings/signing-key)]
        (handler (assoc request :token token) respond raise)
        (respond {:status 401})))))
